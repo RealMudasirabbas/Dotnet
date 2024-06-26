65732f205991fbdf3c22ae2cb16cc95d3cab0af2
Mudasir Abbas <mudasirabbas5638201@gmail.com>
2024-06-26 17:03:40 +0500

    Add project files.

---
+++ ConsoleApp/GuessGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class GuessGame
+    {
+        
+        public int generateSecretNumber()
+        {
+            var randomNumGenerator = new Random();
+            var secretNum = randomNumGenerator.Next(1,100);
+            return secretNum;
+            
+
+        }
+
+        public void guessGame()
+        {
+            var secretNum = generateSecretNumber();
+            Console.WriteLine("Welcome to guess game");
+            Console.WriteLine("Enter your guess");
+
+
+            while (true)
+            {
+
+                var userGuess = Convert.ToInt32(Console.ReadLine());
+
+                if (userGuess == secretNum)
+                {
+                    Console.WriteLine("Congratulations! You won");
+                    break;
+                }
+                else if (userGuess > secretNum)
+                {
+                    Console.WriteLine("Too big");
+                }
+                else if (userGuess < secretNum)
+                {
+                    Console.WriteLine("Too small");
+                }
+            }
+        }
+
+        public double squareOfANumber(int num)
+        {
+           int squareOfANum = Convert.ToInt32( Math.Sqrt(num));
+            Console.WriteLine(squareOfANum);
+            return squareOfANum;
+        }
+
+        
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp;
+using System;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessGame GuessGameObj = new GuessGame();
+            GuessGameObj.guessGame();
+            GuessGameObj.squareOfANumber(6);
+
+          
+        }
+    }
+}
