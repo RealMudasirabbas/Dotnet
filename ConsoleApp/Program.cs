396d3944e8c188806e42d6c6b507d63e75f60007
Mudasir Abbas <mudasirabbas5638201@gmail.com>
2024-06-27 17:01:02 +0500

    add further questions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,8 +10,16 @@ namespace Main
             GuessGame GuessGameObj = new GuessGame();
             RandomQuestions RandomQuestions = new RandomQuestions();
             //GuessGameObj.guessGame();
-            RandomQuestions.PowerOfANumber(6,2);
-            RandomQuestions.AddPy();
+            //RandomQuestions.PowerOfANumber(6,2);
+            //RandomQuestions.AddPy();
+            //RandomQuestions.ArmstrongNumber();
+            int[] numbersArray = { 66,1,3,5,9,79};
+            var result = RandomQuestions.SortAnArray(numbersArray);
+            Console.WriteLine(string.Join(",",result));
+           
+
+            
+            
 
           
         }
--- ConsoleApp/RandomQuestions.cs
+++ ConsoleApp/RandomQuestions.cs
@@ -17,7 +17,7 @@ namespace ConsoleApp
 
         public string AddPy()
         {
-            string userInput = Console.ReadLine();
+            string userInput = Console.ReadLine()!;
             if (userInput.StartsWith("Py") || userInput.StartsWith("py"))
             {
                 Console.WriteLine(userInput);
@@ -30,5 +30,53 @@ namespace ConsoleApp
                 return newStr;
             }
         }
+
+        public int ArmstrongNumber()
+        {
+            Console.WriteLine("Write atleast three digits number: ");
+            string userInput = Console.ReadLine();
+            int num1 = Convert.ToInt32(userInput[0].ToString());
+            int num2 = Convert.ToInt32(userInput[1].ToString());
+            int num3 = Convert.ToInt32(userInput[2].ToString());
+
+            int num1Cube = Convert.ToInt32(Math.Pow(num1, 3));
+            int num2Cube = Convert.ToInt32(Math.Pow(num2, 3));
+            int num3Cube = Convert.ToInt32(Math.Pow(num3, 3));
+            int Sum = num1Cube + num2Cube + num3Cube;
+
+            if (Sum == int.Parse(userInput))
+            {
+                Console.WriteLine("Yes, Its an Armstrong number");
+                
+            }
+            else
+            {
+                Console.WriteLine("No, It is not an Armstrong number");
+               
+            }
+            return 0;
+        }
+
+        public int[] SortAnArray(int[]arr)
+        {
+            for(int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = 0; j < arr.Length-1-i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+
+            return arr;
+
+        }
+       
     }
+   
 }
